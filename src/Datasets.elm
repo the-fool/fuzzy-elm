a08ff4bcf68f1c4edcebd813bf4b75627e315bf4
the-fool <sketchbang@gmail.com>
2016-11-12 06:01:48 -0600

    add xor data generator

---
+++ src/Datasets.elm
@@ -0,0 +1,42 @@
+module Datasets exposing (..)
+
+import Random.Pcg as Random
+import Models exposing (Point)
+
+
+xorData : ( Float, Float ) -> List Point
+xorData ( min, max ) =
+    let
+        label ( x, y ) =
+            if (x * y) >= 0 then
+                ( x, y, 1 )
+            else
+                ( x, y, -1 )
+
+        padding =
+            0.03 * (max - min)
+
+        pad i =
+            if i > 0 then
+                i + padding
+            else
+                i - padding
+    in
+        randomPairs ( min, max ) 100
+            |> List.map (\( x, y ) -> ( pad x, pad y ))
+            |> List.map label
+
+
+seed0 : Random.Seed
+seed0 =
+    Random.initialSeed 628318530 |> Random.step Random.independentSeed |> snd
+
+
+randomPairs : ( Float, Float ) -> Int -> List ( Float, Float )
+randomPairs ( min, max ) len =
+    let
+        gen =
+            Random.pair (Random.float min max) (Random.float min max)
+                |> Random.list len
+    in
+        Random.step gen seed0 |> fst
--- src/SvgViews.elm
+++ src/SvgViews.elm
@@ -1,25 +1,9 @@
 module SvgViews exposing (..)
 
-import Random.Pcg as Random
 import Html exposing (Html)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Models exposing (..)
-
-
-seed0 : Random.Seed
-seed0 =
-    Random.initialSeed 628318530 |> Random.step Random.independentSeed |> snd
-
-
-randomPairs : Int -> List ( Float, Float )
-randomPairs len =
-    let
-        gen =
-            Random.pair (Random.float -1 1) (Random.float -1 1)
-                |> Random.list len
-    in
-        Random.step gen seed0 |> fst
 
 
 largeChart : Float -> List ( Float, Float, Int ) -> Html.Html a
@@ -40,8 +24,3 @@ largeChart dim data =
         svg
             [ version "1.1", x "0", y "0", width <| toString dim, height <| toString dim ]
             (List.map toCircle normalizedData)
-
-
-xorData : List (Int Int Int)
-xorData =
-    []
