6a7bdc861406793aed877f710bfbe9e3597762dd
the-fool <sketchbang@gmail.com>
2016-11-16 19:53:33 -0600

    make canvas message function

--- src/CanvasViz.elm
+++ src/CanvasViz.elm
@@ -1,10 +1,46 @@
 module CanvasViz exposing (..)
 
 import Datasets
+import Network
+import List.Extra
 
 
-toCanvasData : Datasets.AggregatedPredictions -> List (List (List Int))
-toCanvasData =
+type alias RgbaGrid =
+    List (List (List Int))
+
+
+type alias NeuronDatum =
+    { id : String, data : List Int }
+
+
+makeCanvasMessage : Datasets.AggregatedPredictions -> String -> { output : List Int, neurons : List NeuronDatum }
+makeCanvasMessage predictions jumboID =
+    let
+        neurons =
+            predictions |> toRgba |> toCanvasData
+
+        output =
+            case List.Extra.find (.id >> (==) jumboID) neurons of
+                Just item ->
+                    item.data
+
+                Nothing ->
+                    Debug.crash "Unfound ID for jumbo neuron!"
+    in
+        { output = output, neurons = neurons }
+
+
+toCanvasData : RgbaGrid -> List NeuronDatum
+toCanvasData grid =
+    let
+        idDataRecorder id data =
+            { id = id, data = data }
+    in
+        Network.gridPrism idDataRecorder grid |> List.concat
+
+
+toRgba : Datasets.AggregatedPredictions -> RgbaGrid
+toRgba =
     let
         opacity =
             120
