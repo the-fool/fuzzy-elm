08d21bb3d6d7ad11b2dfc7425654b60eb9bf7010
the-fool <sketchbang@gmail.com>
2016-11-24 16:24:29 -0600

    rename constants to core

---
+++ src/Core.elm
@@ -0,0 +1,51 @@
+module Core exposing (..)
+
+import List.Extra exposing (lift2)
+import Random.Pcg as Random
+
+
+colors : { positive : String, negative : String }
+colors =
+    { negative = "rgb(245, 147, 34)"
+    , positive = "rgb(8, 119, 189)"
+    }
+
+
+dataRange : Float
+dataRange =
+    5
+
+
+density : Int
+density =
+    10
+
+
+numInputs : Int
+numInputs =
+    200
+
+
+brutePoints : List ( Float, Float )
+brutePoints =
+    let
+        scale ( min, max ) ( a, b ) x =
+            (b - a) * (x - min) / (max - min) + a
+
+        scaleFun =
+            scale ( 0, toFloat density ) ( -dataRange, dataRange )
+
+        scaledInputs =
+            List.map (toFloat >> scaleFun) (List.range 0 (density - 1))
+    in
+        lift2 (\y x -> ( x, y )) scaledInputs scaledInputs
+
+
+indexedBrutePoints : List ( Int, ( Float, Float ) )
+indexedBrutePoints =
+    List.Extra.zip (List.range 0 (List.length brutePoints)) brutePoints
+
+
+nextSeed : Random.Seed -> Random.Seed
+nextSeed seed =
+    Random.step Random.bool seed |> Tuple.second
